a280fdc0875836af9cdcf0964f172b763b6c3f83
shanlan-L <lixvexi@163.com>
2021-07-25 23:32:50 +0800

    feat: add TailWind & xicons

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <n-space vertical size="large">
-    <n-layout has-sider>
-      <n-layout-sider content-style="padding: 24px;">海淀桥</n-layout-sider>
-      <n-layout>
-        <n-layout-header>颐和园路</n-layout-header>
-        <n-layout-content content-style="padding: 24px"
-          >平山道</n-layout-content
-        >
-        <n-layout-footer>成府路</n-layout-footer>
-      </n-layout>
+  <n-layout class="h-screen" has-sider>
+    <n-layout-sider class="bg-green-100" content-style="padding: 24px;">
+      <n-input round placeholder="搜索">
+        <template #suffix>
+          <n-icon>
+            <file-search />
+          </n-icon>
+        </template>
+      </n-input>
+    </n-layout-sider>
+    <n-layout>
+      <n-layout-header class="h-1/6 bg-blue-100">标题</n-layout-header>
+      <n-layout-content
+        class="h-5/6 bg-purple-100"
+        content-style="padding: 24px"
+        >内容</n-layout-content
+      >
     </n-layout>
-  </n-space>
+  </n-layout>
 </template>
 
 <script>
 import { NSpace, NLayout } from 'naive-ui'
 import { defineComponent } from 'vue'
+import { Search as FileSearch } from '@vicons/ionicons5'
+
 export default defineComponent({
   name: 'App',
   components: {
     NSpace,
-    NLayout
+    NLayout,
+    FileSearch
   }
 })
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import Naive from 'naive-ui'
+import './index.css'
 
 createApp(App).use(Naive).mount('#app')
